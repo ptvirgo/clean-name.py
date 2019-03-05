32ed994af7d970ef5cef708cb53005f13db8e93a
Pablo Virgo <mailbox@pablovirgo.com>
2019-03-04 21:21:46 -0500

    Updated to current coding style.

--- clean_name.py
+++ clean_name.py
@@ -5,70 +5,75 @@ import os.path
 import shutil
 import re
 
-'''Write a program that renames files specified at runtime.  The new name
+
+"""Write a program that renames files specified at runtime.  The new name
 should be lower case and characters that require escaping in BASH should be
-removed or replaced.'''
+removed or replaced."""
+
 
 def clean_name(fn):
-  """Clean up a file name.
+    """
+    Clean up a file name.
 
     Parameters:  a file name, possibly including the absolute path
     Returns:  the file name with an intact path, case corrected and cleaned-up
-    filename."""
+    filename.
+    """
 
-  path = unix_split_basename(fn)
+    path = unix_split_basename(fn)
+    file_name = path[1]
 
-  andpersand = re.compile(r'&')
-  whitespace = re.compile(r'[\s]')
+    file_name = re.sub("&", "_and_", file_name)
+    file_name = re.sub("[\s]+", "_", file_name)
 
-  # Replace whitespace and andpersands with underscores and "_and_"
-  file_name = whitespace.sub("_", andpersand.sub("_and_", path[1]))
+    # strip remaining escape characters
+    file_name = re.sub("[^\w\.\~-]", "", file_name)
 
-  # strip remaining escape characters
-  remove = re.compile(r'[^\w\.\~-]')
-  file_name = remove.sub("", file_name)
+    # dedup _ and .
 
-  # dedup _ and .
+    file_name = re.sub("_+", "_", file_name)
+    file_name = re.sub("[_\.]*\._*", ".", file_name)
 
-  periods = re.compile(r'\.\.+')
-  underscores = re.compile(r'__+')
-
-  file_name = periods.sub(".", file_name)
-  file_name = underscores.sub("_", file_name)
-
-  return os.path.join(path[0], file_name.lower())
+    return os.path.join(path[0], file_name.lower())
 
 
 def unix_split_basename(fn):
-  """Split a filename the same way an old-school unix basename would. A dir is
-  still a file, we just want to split the parents from the last child.
+    """
+    Split a filename the same way an old-school unix basename would. A dir is
+    still a file, we just want to split the parents from the last child.
 
-  Parameters: a file name, preferably including absolute path
-  Returns: tuple of the unix-style directory and basename for the provided
-  file.  Unix style meaning that unlike os.path.basename, "/foo/bar/"
-  returns ("/foo", "bar")."""
+    Parameters: a file name, preferably including absolute path
+    Returns: tuple of the unix-style directory and basename for the provided
+    file.  Unix style meaning that unlike os.path.basename, "/foo/bar/"
+    returns ("/foo", "bar").
+    """
 
-  names = fn.split("/")
+    names = fn.split("/")
 
-  basename = names.pop()
-  while (basename == ''):
     basename = names.pop()
-  dirs = "/".join(names)
 
-  return (dirs, basename)
+    while (basename == ''):
+        basename = names.pop()
+
+    dirs = "/".join(names)
+
+    return (dirs, basename)
+
 
 def main():
-  """Actually rename the files as specified on the command line.  This defines
-  the process-flow for the script."""
+    """
+    Rename the files as specified on the command line.
+    """
 
-  parser = argparse.ArgumentParser(
-    description='Rename files to be lower case and free of escaped characters.')
-  parser.add_argument('file_names', help="List of files to be renamed", nargs='+')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Rename files with unix style names.")
+    parser.add_argument('file_names', help="List of files to be renamed", nargs='+')
+    args = parser.parse_args()
 
-  for fn in args.file_names:
-    nn = clean_name(fn)
-    shutil.move(fn, nn)
+    for fn in args.file_names:
+        nn = clean_name(fn)
+        shutil.move(fn, nn)
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- test.py
+++ test.py
@@ -1,36 +1,42 @@
 import unittest
-from clean_name import *
+from clean_name import clean_name, unix_split_basename
 
-'''
-Data Definitions:
-'''
-
-# Filename is string
-
-FN1 = "/tmp/file.txt"
-FN2 = "~/Downloads"
-
-def function_for_filename(fn):
-  # ... 
-  return fn
 
 class TestCleanName(unittest.TestCase):
 
-  def test_clean_name(self):
-    self.assertEqual(clean_name( "/HOME/USER/DOCUMENTS" ),
-      "/HOME/USER/documents")
-    self.assertEqual(clean_name( "/home/user/documents/noTes-2.TXT"),
-      "/home/user/documents/notes-2.txt")
-    self.assertEqual(clean_name(
-      "/home/user/documents/N#o%p!`e(&)_thA$nks...TXT~"),
-      "/home/user/documents/nope_and_thanks.txt~")
-    self.assertEqual(clean_name( "/home/user/documents/so...much...win"),
-      "/home/user/documents/so.much.win")
+    def test_clean_name_lowercases(self):
+        """clean_name() should lowercase file names"""
+        self.assertEqual(
+            clean_name("/HOME/USER/DOCUMENTS"), "/HOME/USER/documents")
 
-  def test_unix_split_basename(self):
-    self.assertEqual(unix_split_basename("/foo/bar/"), ("/foo", "bar"))
-    self.assertEqual(unix_split_basename("/foo/bar/baz"), ("/foo/bar", "baz"))
-    self.assertEqual(unix_split_basename("foo"), ("", "foo"))
+        self.assertEqual(
+            clean_name("/home/user/documents/noTes-2.TXT"),
+            "/home/user/documents/notes-2.txt")
+
+    def test_clean_name_character_cleanup(self):
+        """clean_name() should drop weird characters"""
+
+        self.assertEqual(clean_name(
+            "/home/user/documents/N#o%p!`e(&)_thA$nks...TXT~"),
+            "/home/user/documents/nope_and_thanks.txt~")
+
+        self.assertEqual(
+            clean_name("/home/user/documents/so...much...win"),
+            "/home/user/documents/so.much.win")
+
+        self.assertEqual(
+            clean_name("/home/user/something_.txt"),
+            "/home/user/something.txt")
+
+        self.assertEqual(
+            clean_name("/home/user/something._txt"),
+            "/home/user/something.txt")
+
+    def test_unix_split_basename(self):
+        self.assertEqual(unix_split_basename("/foo/bar/"), ("/foo", "bar"))
+        self.assertEqual(unix_split_basename("/foo/bar/baz"), ("/foo/bar", "baz"))
+        self.assertEqual(unix_split_basename("foo"), ("", "foo"))
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
